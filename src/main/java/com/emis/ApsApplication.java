f4bbd830d351889f6c626bd3bd0de473f14a7d48
chenzihao03@gmail.com <zhuzhubaobei520>
2020-09-14 13:56:25 +0800

    12

--- src/main/java/com/emis/ApsApplication.java
+++ src/main/java/com/emis/ApsApplication.java
@@ -2,10 +2,17 @@ package com.emis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApsApplication {
+public class ApsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApsApplication.class);
     }
 }
